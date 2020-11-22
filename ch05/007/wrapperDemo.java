87af007bfb5039fabc1759d48070e571ccbc0098
9501sam <44914572+9501sam@users.noreply.github.com>
2020-11-23 02:34:05 +0800

    Add files via upload

---
+++ ch05/007/wrapperDemo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class wrapperDemo {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		String inputStr,outputStr;
+		int inputInt,outputInt;
+		inputStr=sc.next();
+		inputInt=sc.nextInt();
+
+		outputInt = Integer.valueOf(inputStr) + 3;
+		outputStr = Integer.toString(inputInt) + "3";
+
+		System.out.println(outputInt);
+		System.out.print(outputStr);
+	}
+}
---
+++ ch05/008/MyMain.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class MyMain{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		String all = sc.nextLine();
+		Scanner haha = new Scanner(all);
+		String first = haha.next();
+		all = all.substring(first.length());
+
+		System.out.print(first + all.toUpperCase());
+	}
+}
